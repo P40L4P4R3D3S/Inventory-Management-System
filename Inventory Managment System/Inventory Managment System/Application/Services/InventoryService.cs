294321f0fd14888420cef715054aac4501d7088e
Paola.Paredes <paolatigreparedes@gmail.com>
2026-07-13 12:31:12 -0400

    (feat): Add first structure project to repo

---
+++ Inventory Managment System/Inventory Managment System/Application/Services/InventoryService.cs
@@ -0,0 +1,11 @@
+﻿using Inventory_Managment_System.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Managment_System.Application.Services
+{
+    internal class InventoryService: IInventoryService
+    {
+    }
+}
---
+++ Inventory Managment System/Inventory Managment System/Domain/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory_Managment_System.Domain.Models
+{
+    internal class Product
+    {
+        private string _id;
+        private string _name;
+        private string _description;
+        private decimal _price;
+        private string _sku;
+        private int _quantityOnHand;
+
+    }
+}
